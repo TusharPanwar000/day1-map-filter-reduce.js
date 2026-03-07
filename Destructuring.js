8b1567cecbb8bad9c886d7fea1c431583a0ec56a
Tushar Panwar <tusharpanwar0000@gmail.com>
2026-03-07 13:27:31 +0530

    Day 3: Learned ES6 features

---
+++ Destructuring.js
@@ -0,0 +1,72 @@
+let person = {
+  name: "Tushar",
+  age: 22,
+  city: "Meerut"
+};
+
+// let name = person.name;
+// let age = person.age;
+// let city = person.city;
+
+// let { name , age, city} = person; 
+
+
+// console.log(name, age, city);
+
+
+// let colors = ["Red", "Green", "Blue"];
+
+// let [first, second, third] = colors;
+
+// console.log(first, second, third);
+
+
+// let name  = ["Tushar", "Rahul", "Priya", "Rohit"];
+
+// let [first, second] = name;
+
+// console.log(first, second);
+
+
+// let arr1 = [1,2,3];
+// let arr2 = [4,5,6];
+
+
+// let combined = [...arr1, ...arr2];
+// console.log(combined);
+
+// let person1 = {name: "Tushar", age: 22};
+// let person2 = {city: "Meerut", job: "Developer"};
+ 
+
+// let combined = {...person1, ...person2};
+
+// console.log(combined);
+
+
+// let fruits = ["Apple", "Mango", "Banana"];
+// let veggis = ["Potato", "Tomato", "Onion"];
+
+// let combined = [...fruits,  "Guava",...veggis];
+
+// console.log(combined);
+
+
+//Template Litrals 
+
+
+
+// let name = "Tushar";
+// let city = "Meerut";
+
+
+// console.log("Mera naam " + name + " hai aur mein " + city + " se hu");
+
+// console.log(`Mera naam ${name} hai aur mein ${city} se hu`)
+
+
+let product = "Laptop";
+let price = 50000;
+let discount = 5000;
+
+console.log(`Laptop ki price ${price} hai aur discount ke baad ${price - discount} mein melega`)
---
+++ app.js
@@ -0,0 +1,7 @@
+// import {add, multiply } from "./math.js";
+// console.log(add(5,3));
+// console.log(multiply(5,3));
+
+import subtract from "./math.js";
+
+console.log(subtract(10,3));
